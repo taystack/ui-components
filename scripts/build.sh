6aa8d29857d797542efd0bed7f5c8ebf9098144f
Taylor Stackpole <tay.stack@gmail.com>
2019-01-24 12:27:34 -0800

    Updating script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+echo """
+Make sure you commit your changes and push develop
+"""
+
 git checkout master && \
 git pull origin develop &&\
 ./node_modules/.bin/build-storybook -- --dry-run -c .storybook
+git push origin master
+git checkout develop
 
 mv storybook-static docs
