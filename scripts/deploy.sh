2839fcd99e6ad9c76b084509832c8aea663b1a87
Taylor Stackpole <tay.stack@gmail.com>
2019-01-24 13:27:00 -0800

    scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
 echo """
-
   Removing images/, src/, stories/
-
 """
 rm -rf images src stories
 
 echo """
-
   Moving storybook-static/ to docs/
-
 """
 rm -rf docs
 mv storybook-static docs
 
 echo """
 
-Moving storybook-static/ to docs/
+  Deploying GitHub page
 
 """
 git add . &&\
